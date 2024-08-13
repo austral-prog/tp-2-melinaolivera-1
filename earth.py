3b93185bfe34168043be9272920cdc0b74da2776
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-13 15:29:07 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+   Y = "barbados "
+   X = "Bangladesh"
+   comparacion = X > Y
+   otra_comparacion = Y > X
+   print(f' The result of X comes first in the diccionary than Y is {comparacion}')
+   print(f'The result of Y comes first in the diccionary than X is {otra_comparacion}')
