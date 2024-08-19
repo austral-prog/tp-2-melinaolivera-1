9494ce6b95fabf5834466abadae8a40596b7e8dc
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 15:58:17 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,7 +1,7 @@
-def change():
-    gasto = float(input("Ingresar gasto:\n"))
-    dinero_recibido = float(input("Dinero recibido\n"))
-    vuelto = dinero_recibido - gasto
-    pesos = int(vuelto)
-    centavos = int((vuelto - pesos) * 100)
-    print(f"\nVuelto\n\nPesos:\n{pesos}\nCentavos:\n{centavos}")
+def earth():
+    country1 = "Bangladesh"
+    country2 = "Barbados"
+    first_country_comes_first = country1 < country2
+    second_country_comes_first = country2 < country1
+    print(f"The result of {country1} comes first in the dictionary than {country2} is {first_country_comes_first}.")
+    print(f"The result of {country2} comes first in the dictionary than {country1} is {second_country_comes_first}.")
