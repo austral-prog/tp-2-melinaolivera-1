7a86b04fc00f4dfdbb8df05ad1979ea6e1a442ef
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 16:37:12 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,5 +1,5 @@
 def earth():
     country1 = "Bangladesh"
     country2 = "Barbados"
-    print(f"{country1} comes first in the dictionary than {country2}: {country1 < country2}")
-    print(f"{country2} comes first in the dictionary than {country1}: {country2 > country1}")
+    print(f"The result of {country1} comes first in the dictionary than {country2} is {country1 < country2}")
+    print(f"The result of {country2} comes first in the dictionary than {country1} is {country2 > country1}")
