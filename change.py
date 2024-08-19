70791923e24244282fd5c028c353f9db4f451be8
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 15:44:18 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,16 +1,9 @@
-
-def calcular_vuelto(gasto, recibido):
-    vuelto_total = recibido - gasto
-    pesos = int(vuelto_total)
-    centavos = int(round((vuelto_total - pesos) * 100))
-    print("Ingresar gasto:")
-    print(f"{gasto:.2f}")
-    print("Dinero recibido")
-    print(f"{recibido:.2f}")
-    print("")  # Línea vacía
-    print("Vuelto")
-    print("")  # Línea vacía
-    print("Pesos:")
-    print(f"{pesos}")
-    print("Centavos:")
-    print(f"{centavos}")
+def change():
+    gasto = float(input("Ingresar gasto:\n"))
+dinero_recibido = float(input("Dinero recibido\n"))
+vuelto = dinero_recibido - gasto
+pesos = int(vuelto)
+centavos = round((vuelto - pesos) * 100)
+print("\nVuelto\n")
+print(f"Pesos:\n{pesos}")
+print(f"Centavos:\n{centavos}")
