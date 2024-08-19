1dacc58cf0bdd5a05f51be38b7673b33b4f9e869
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 20:58:46 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,8 +1,7 @@
 def earth():
     country_x = "Bangladesh"
     country_y = "Barbados"
-    sorted_countries = sorted([country_x, country_y])
-    result_x_first = (sorted_countries[0] == country_x)
-    result_y_first = (sorted_countries[0] == country_y)
+    result_x_first = (country_x < country_y)
+    result_y_first = (country_y < country_x)
     print(f"The result of {country_x} comes first in the dictionary than {country_y} is {result_x_first}.")
     print(f"The result of {country_y} comes first in the dictionary than {country_x} is {result_y_first}.")
