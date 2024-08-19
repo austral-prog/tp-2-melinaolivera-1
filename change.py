fbc608960bad54b13cdb52f95cf1b08b6c957a11
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 15:50:27 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,9 +1,7 @@
 def change():
     gasto = float(input("Ingresar gasto:\n"))
-dinero_recibido = float(input("Dinero recibido\n"))
-vuelto = dinero_recibido - gasto
-pesos = int(vuelto)
-centavos = round((vuelto - pesos) * 100)
-print("\nVuelto\n")
-print(f"Pesos:\n{pesos}")
-print(f"Centavos:\n{centavos}")
+    dinero_recibido = float(input("Dinero recibido\n"))
+    vuelto = dinero_recibido - gasto
+    pesos = int(vuelto)
+    centavos = int((vuelto - pesos) * 100)
+    print(f"\nVuelto\n\nPesos:\n{pesos}\nCentavos:\n{centavos}")
