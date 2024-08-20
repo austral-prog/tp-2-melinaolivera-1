500dc41d29da4cf8730094fda16382f5fc8d22b9
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 23:09:37 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,14 +1,11 @@
 def imprimir_informe_vuelto():
-    # Solicitar entrada del usuario
     gasto = float(input("Ingresar gasto:\n"))
     dinero_recibido = float(input("Dinero recibido\n"))
 
-    # Calcular el vuelto
     vuelto = dinero_recibido - gasto
     pesos = int(vuelto)
     centavos = int(round((vuelto - pesos) * 100))
 
-    # Imprimir el informe
     print("\nVuelto\n")
     print(f"Pesos:\n{pesos}")
     print(f"Centavos:\n{centavos}")
