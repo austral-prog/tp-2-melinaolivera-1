c356c52c6171959e273dcd5ff5d130965a068cff
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 22:55:59 -0300

    Update change.py

--- change.py
+++ change.py
@@ -4,7 +4,7 @@ def comparar_paises(pais_x, pais_y):
     # Determina si pais_y viene antes que pais_x en el diccionario
     resultado_y_primero = pais_y < pais_x
 
-    # Imprime los resultados con el formato requerido
+    # Imprimir resultados con el formato requerido
     print(f"The result of {pais_x} comes first in the dictionary than {pais_y} is {resultado_x_primero}.")
     print(f"The result of {pais_y} comes first in the dictionary than {pais_x} is {resultado_y_primero}.")
 
