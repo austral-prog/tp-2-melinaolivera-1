22dd2284b3c8f7bfb5f9ed8e6cdfd740474e82b2
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 22:25:12 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,15 +1,15 @@
-import sys
+import unittest
 
-def main(gasto, dinero_recibido):
+def calcular_vuelto(gasto, dinero_recibido):
     vuelto = dinero_recibido - gasto
     pesos = int(vuelto)
     centavos = int(round((vuelto - pesos) * 100))
+    return pesos, centavos
 
-    print("\nVuelto")
-    print(f"Pesos: {pesos}")
-    print(f"Centavos: {centavos}")
+class TestVuelto(unittest.TestCase):
+    def test_vuelto(self):
+        self.assertEqual(calcular_vuelto(100, 150), (50, 0))
+        self.assertEqual(calcular_vuelto(99.75, 100), (0, 25))
 
-if __name__ == "__main__":
-    gasto = float(sys.argv[1])
-    dinero_recibido = float(sys.argv[2])
-    main(gasto, dinero_recibido)
+if __name__ == '__main__':
+    unittest.main()
