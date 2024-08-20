1dc9d082fc9392094d478cfd239c5bcab3840a2a
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-20 16:51:53 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,12 +1,13 @@
-first_name = "AdA"
-last_name = "LoVeLAce"
+def ada():
+    first_name = "AdA"
+    last_name = "LoVeLAce"
 
 
-first_name_normalized = first_name.lower()
-last_name_normalized = last_name.lower()
+    first_name_normalized = first_name.lower()
+    last_name_normalized = last_name.lower()
 
-formatted1 = f"{first_name_normalized} {last_name_normalized}"
-formatted2 = f"{first_name.capitalize()} {last_name.capitalize()}"
-formatted3 = f"{first_name.upper()} {last_name.upper()}"
-formatted4 = f"\t{first_name_normalized} {last_name_normalized}"
+    formatted1 = f"{first_name_normalized} {last_name_normalized}"
+    formatted2 = f"{first_name.capitalize()} {last_name.capitalize()}"
+    formatted3 = f"{first_name.upper()} {last_name.upper()}"
+    formatted4 = f"\t{first_name_normalized} {last_name_normalized}"
 
