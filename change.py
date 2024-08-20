2e99b7e185dae74a445fdbb88e08bb7365c2a293
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 22:47:57 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,17 +1,11 @@
-    print("Ingresar gasto:")
-    gasto = float(input())
-    
-    print("Dinero recibido")
-    dinero_recibido = float(input())
+def comparar_paises(pais_x, pais_y):
+    resultado_x_primero = pais_x < pais_y
+    resultado_y_primero = pais_y < pais_x
 
-    vuelto = dinero_recibido - gasto
-    pesos = int(vuelto)
-    centavos = int(round((vuelto - pesos) * 100))
+    print(f"The result of {pais_x} comes first in the dictionary than {pais_y} is {resultado_x_primero}.")
+    print(f"The result of {pais_y} comes first in the dictionary than {pais_x} is {resultado_y_primero}.")
 
-    print("")  
-    print("Vuelto")
-    print("")  
-    print(f"Pesos:")
-    print(f"{pesos}")
-    print(f"Centavos:")
-    print(f"{centavos}")
+pais_x = "Bangladesh"
+pais_y = "Barbados"
+
+comparar_paises(pais_x, pais_y)
