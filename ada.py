f3949ff877ccd060e468fbfc8b2c0dd3c6bae9f7
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-19 22:01:20 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,10 +1,12 @@
 def ada():
-   first_name = "AdA"
-   last_name = "LoVeLAce"
-   lowercase_name = f"{first_name.lower()} {last_name.lower()}"
-   capitalized_name = f"{first_name.title()} {last_name.title()}"
-   uppercase_name = f"{first_name.upper()} {last_name.upper()}"
-   print(lowercase_name)
-   print(capitalized_name)
-   print(uppercase_name)
-   print(lowercase_name, end='\n')
+    first_name = "AdA"
+    last_name = "LoVeLAce"
+
+    lowercase_name = f"{first_name.lower()} {last_name.lower()}"
+    capitalized_name = f"{first_name.capitalize()} {last_name.capitalize()}"
+    uppercase_name = f"{first_name.upper()} {last_name.upper()}"
+
+    print(lowercase_name)
+    print(capitalized_name)
+    print(uppercase_name)
+   print(f"\n{lowercase_name}")
