ba36f42c29fa68b7d5ed501d523929d585a139d2
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-20 17:16:01 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,7 +1,4 @@
 def earth():
-    pais1 = "Bangladesh"
-    pais2 = "Barbados"
-    pais1_antes_que_pais2 = pais1 < pais2
-    pais2_antes_que_pais1 = pais2 < pais1
-    print(f"The result of {pais1} comes first in the dictionary than {pais2} is {pais1_antes_que_pais2}.")
-    print(f"The result of {pais2} comes first in the dictionary than {pais1} is {pais2_antes_que_pais1}.")
+    x = "Bangladesh"
+    y = "Barbados"
+    print(f"The result of {x} comes first in the dictionary than {y}  is {x<y}.\nThe result of {y} comes first in the dictionary than {x} is {y<x}.")
